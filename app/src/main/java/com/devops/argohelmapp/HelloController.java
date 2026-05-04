c69b6f0baea0181c68114181a7792e4c8fcd72f6
Srikanth <sri.kranthi187@gmail.com>
2026-05-05 03:01:36 +0530

    Updated code for deployment test

--- app/src/main/java/com/devops/argohelmapp/HelloController.java
+++ app/src/main/java/com/devops/argohelmapp/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "ArgoHelm DevOps Project 🚀";
+        return "ArgoHelm DevOps Project2 🚀";
     }
 
     @GetMapping("/health")
